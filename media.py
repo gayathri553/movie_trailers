0dde6a9cf5afbc9b937605b167e8e737efe3b919
gayathri553 <37368700+gayathri553@users.noreply.github.com>
2018-05-23 10:31:33 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,12 +1,12 @@
-print("Content-type:text/html \n")
 import webbrowser
+print("Content-type:text/html \n")
 
 
 class Movie():
-    VALID_RATINGS=["Excellent","Good","Average","Ok"]
-    def __init__(i,movie_title,poster_image,trailer_youtube):
-        i.title=movie_title
-        i.poster_image_url=poster_image
-        i.trailer_youtube_url=trailer_youtube
-    def show_trailer(i):
-        webbrowswer.open(i.trailer_youtube_url)
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowswer.open(self.trailer_youtube_url)
